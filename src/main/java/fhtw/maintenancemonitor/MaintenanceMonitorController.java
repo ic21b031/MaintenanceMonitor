b3c5f5ccff7fc218b75e8f7decd5a1945d4db112
RiederPeter <32160760+RiederPeter@users.noreply.github.com>
2022-11-25 10:46:00 +0100

    api/message/set function implemented

--- src/main/java/fhtw/maintenancemonitor/MaintenanceMonitorController.java
+++ src/main/java/fhtw/maintenancemonitor/MaintenanceMonitorController.java
@@ -1,12 +1,19 @@
 package fhtw.maintenancemonitor;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MaintenanceMonitorController {
     private final MaintenanceMessage maintenanceMessage = new MaintenanceMessage("Everything works as expected", false);
 
+    @GetMapping("/api/message/set")
+    public String _setMessage(@RequestParam(value = "m") String m) {
+        maintenanceMessage.setMessage(m);
+        maintenanceMessage.setAlarm(!m.equals("Everything works as expected"));
+        return "ok";
+    }
     @GetMapping("/api/message/reset")
     public String _resetMessage() {
         maintenanceMessage.setMessage("Everything works as expected");
